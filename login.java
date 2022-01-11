13a81cb9a40ed96414848996a45cacdf081048c0
unknown <shubhangi.mali@wissen.com>
2022-01-11 09:39:16 +0530

    login file changepassword method updated

--- login.java
+++ login.java
@@ -2,7 +2,8 @@ public class Login{
 public static void main(String args[]){
 
 public void changePassword(){
-System.out.println("change password implementation");
+System.out.println("changing password implementation");
+System.out.println("changing password");
 }
 System.out.println("username & password");
 }
